491cdde04dfca7f627e6b49b610462cc73ec65a3
Marshall Christian <marstheprogrammer@gmail.com>
2021-09-14 20:01:06 -0500

    worked on getting Appointments class set up

---
+++ src/model/Appointments.java
@@ -0,0 +1,119 @@
+package model;
+
+import java.time.LocalTime;
+
+public class Appointments {
+
+    private int appointmentId;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private String start;
+    private String end;
+    private int customerId;
+    private int userId;
+    private String contactId;
+
+    //private String customerName;
+    //private LocalTime time;
+
+    //Basic constructor in the event we need it
+    public Appointments() {}
+
+    //Constructor
+    public Appointments(int appointmentId, String title, String description, String location, String type,
+                        String start, String end, int customerId, int userId, String contactId) {
+        this.appointmentId = appointmentId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.customerId = customerId;
+        this.userId = userId;
+        this.contactId = contactId;
+    }
+
+    //Getters and Setters
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+}
